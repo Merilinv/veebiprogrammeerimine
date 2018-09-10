7198e939d196631317d771830ceda88cc6c0f8f2
Merilin Võrk <merilinv@users.noreply.github.com>
2018-09-10 10:40:53 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Merilin Võrk, õppetöö</title>
+</head>
+<body>
+  <h1>Merilin Võrk</h1>
+  <p>See leht on valminud <a HREF="http://WWW.tlu.ee" target="_blank">
+  TLÜ</a>õppetöö raames. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+  
+  <p><b><i>Mina olen Tallinna Ülikooli värske tudeng Merilin! Mulle meeldib maalida ja sporti teha, jäätis meeldib mulle ka...</i></b></p>
+  
+  
+  <!--<img src=
+  "http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+  <img src=
+  "../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+
+  <p> Mul on sõber, kes ka teeb veebi. <a href= "../../~darikre"</a> </p>
+  
+</body>
+</html>	
